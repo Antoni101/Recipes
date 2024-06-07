e0ce589b3b9c1fdf7dcc7e47c32a0f81b4600b61
Antoni101 <abekhet2023@gmail.com>
2024-06-07 17:11:53 -0400

    Finished everything else

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickImage(imageNum) {
+    if (imageNum == 1) { window.location.href = "./recipe-pages/shawarma.html"; }
+    else if (imageNum == 3) { 
+        window.location.href = "./recipe-pages/chick-enc.html"; 
+    }
+    else if (imageNum == 2) { 
+        window.location.href = "./recipe-pages/broc-ched.html"; 
+    }
+}
